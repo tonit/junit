0e7f638d6b69ace882d0e565798c77f5d9dba535
unknown <berin.loritsch@.ad.gd-ais.com>
2010-03-01 23:23:48 +0800

    Add more information to the Runner JavaDocs

--- src/main/java/org/junit/runners/package-info.java
+++ src/main/java/org/junit/runners/package-info.java
@@ -1,5 +1,15 @@
 /**
- * Provides standard {@link org.junit.runner.Runner Runner} implementations.
+ * Provides standard {@link org.junit.runner.Runner Runner} implementations.  These runners
+ * enable different sets of functionality:
+ *
+ * <ul>
+ * <li>{@link JUnit4} - standard JUnit 4 tests, this is the default runner so you don't
+ *    have to specify it directly.</li>
+ * <li>{@link Parameterized} - parameterized tests that provide a set of test parameters
+ *    for the test to be run against.</li>
+ * <li>{@link Suite} - allows you to collect a group of test cases together to run with
+ *    one class.</li>
+ * </ul>
  *
  * @since 4.0
  * @see org.junit.runner.Runner
